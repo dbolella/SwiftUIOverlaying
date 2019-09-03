f8f6f33b5eb39ff5f7d96052389bc27817515055
Danny Bolella <dbolella@gmail.com>
2019-09-03 17:07:02 -0400

    Initial

--- Overlaying/ContentView.swift
+++ Overlaying/ContentView.swift
@@ -9,8 +9,53 @@
 import SwiftUI
 
 struct ContentView: View {
+//    @State var showOverLay = false
+    @State var grow = false
+    @State var showOverlay = false
+    @State var curColor = Color.red
+    
+    let colors = [Color.blue, Color.red, Color.green, Color.yellow]
+    
     var body: some View {
         Text("Hello World")
+            .frame(width: 100, height: 100)
+            .background(curColor)
+            .onTapGesture { self.showOverlay.toggle() }
+            .overlay(
+                VStack {
+                    if self.showOverlay {
+                        ZStack {
+                            ForEach(0 ..< 4) { item in
+                                ArcSelectionView(isShowing: self.grow, arcPosition: (0.25 * Double(item + 1)), color: self.colors[item])
+                                    .onTapGesture {
+                                        self.curColor = self.colors[item]
+                                        self.showOverlay.toggle()
+                                }
+                            }
+                        }
+                        .rotationEffect(.degrees(self.grow ? 0 : 180))
+                        .animation(.linear(duration: 0.5))
+                        .onAppear { self.grow.toggle() }
+                        .onDisappear { self.grow.toggle() }
+                    } else {
+                        EmptyView()
+                    }
+                }
+        )
+    }
+}
+
+struct ArcSelectionView: View {
+    var isShowing = false
+    var arcPosition : Double
+    var color : Color
+    
+    var body: some View {
+        Circle()
+            .trim(from: isShowing ? CGFloat(arcPosition - 0.25) : CGFloat(arcPosition), to: CGFloat(arcPosition))
+            .stroke(color, lineWidth: 30)
+            .frame(width: 300, height: 300)
+            .animation(.linear(duration: 0.4))
     }
 }
 
