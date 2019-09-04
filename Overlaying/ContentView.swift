dc74389fdf8a5252ced6233959fad284273d00cc
Danny Bolella <dbolella@gmail.com>
2019-09-04 15:16:46 -0400

    Pass State through to Bindable

--- Overlaying/ContentView.swift
+++ Overlaying/ContentView.swift
@@ -12,47 +12,40 @@ struct ContentView: View {
     @State var showOverlay = false
     @State var curColor = Color.blue
     
-    let colors = [Color.blue, Color.red, Color.green, Color.yellow]
-    
     var body: some View {
         Text("Hello World")
             .frame(width: 100, height: 100)
             .background(curColor)
             .cornerRadius(20)
             .onTapGesture { self.showOverlay.toggle() }
-            .overlay(
-                VStack {
-                        ZStack {
-                            ForEach(0 ..< 4) { item in
-                                ArcSelectionView(isShowing: self.showOverlay,
-                                                 arcPosition: (0.25 * Double(item + 1)),
-                                                 color: self.colors[item])
-                                    .onTapGesture {
-                                        self.curColor = self.colors[item]
-                                        self.showOverlay.toggle()
-                                }
-                            }
-                        }
-                        .rotationEffect(.degrees(self.showOverlay ? 0 : 180))
-                        .animation(.linear(duration: 0.5))
-                        .opacity(self.showOverlay ? 1 : 0)
-                }
-        )
+            .overlay( ArcSelectionView(isShowing: self.$showOverlay, curColor: self.$curColor) )
     }
 }
 
 struct ArcSelectionView: View {
-    var isShowing = false
-    var arcPosition : Double
-    var color : Color
+    @Binding var isShowing : Bool
+    @Binding var curColor : Color
+    
+    let colors = [Color.blue, Color.red, Color.green, Color.yellow]
     
     var body: some View {
-        Circle()
-            .trim(from: isShowing ? CGFloat(arcPosition - 0.25) : CGFloat(arcPosition),
-                  to: CGFloat(arcPosition))
-            .stroke(color, lineWidth: 30)
-            .frame(width: 300, height: 300)
-            .animation(.linear(duration: 0.4))
+        ZStack {
+            ForEach(1 ..< 5, id: \.self) { item in
+                Circle()
+                    .trim(from: self.isShowing ? CGFloat((Double(item) * 0.25) - 0.25) : CGFloat(Double(item) * 0.25),
+                          to: CGFloat(Double(item) * 0.25))
+                    .stroke(self.colors[item - 1], lineWidth: 30)
+                    .frame(width: 300, height: 300)
+                    .animation(.linear(duration: 0.4))
+                    .onTapGesture {
+                        self.curColor = self.colors[item - 1]
+                        self.isShowing.toggle()
+                }
+            }
+        }
+        .opacity(self.isShowing ? 1 : 0)
+        .rotationEffect(.degrees(self.isShowing ? 0 : 180))
+        .animation(.linear(duration: 0.5))
     }
 }
 
